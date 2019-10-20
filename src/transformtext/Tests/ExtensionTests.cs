5072d15c84af6432892f604f3dfc2560b5cd2a55
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2019-10-20 21:57:09 +0200

    add NUnit to same project

---
+++ src/transformtext/Tests/ExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace transformtext.Tests {
+
+    class ExtensionTests {
+
+        [SetUp]
+        public void Setup() {
+
+        }
+
+        [TestCase]
+        public void TestMethod() {
+
+            Assert.Pass();
+        }
+    }
+}
